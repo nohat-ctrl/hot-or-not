bb8b25d1c4333500510024c84ed5fafa7a782903
nohat-ctrl <59539406+nohat-ctrl@users.noreply.github.com>
2020-01-05 20:42:06 +0100

    Add files via upload

---
+++ model.py
@@ -0,0 +1,27 @@
+import tensorflow as tf
+import pickle
+import time
+import numpy
+
+pickle_in = open("X.pickle","rb")
+X = pickle.load(pickle_in)
+
+pickle_in = open("y.pickle","rb")
+y = pickle.load(pickle_in)
+y = numpy.array(y)
+print(type(y))
+X = X/255.0
+
+model = tf.keras.models.Sequential()
+model.add(tf.keras.layers.Flatten())
+model.add(tf.keras.layers.Dense(256, activation=tf.nn.relu))
+model.add(tf.keras.layers.Dense(256, activation=tf.nn.relu))
+model.add(tf.keras.layers.Dense(5, activation=tf.nn.softmax))
+
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(X, y, epochs=20)
+
+model.save("beauty_checker.model")
